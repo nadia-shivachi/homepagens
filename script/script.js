03e84659f34e56ec0ccf75d1b89a3a69904ecff6
Nadia Shivachi <nadia.shivachi@gmail.com>
2022-08-11 15:36:55 +0200

    JS function changed

--- script/script.js
+++ script/script.js
@@ -1,8 +1,11 @@
 console.log("Hej! strona o mnie. Zapraszam do zapoznania się :)")
 
+let imageElement = document.querySelector(".js-image");
+let buttonElement = document.querySelector(".js-button");
+
 let photo1 = "https://i.postimg.cc/JnCDkf3B/IMG-20160831-173342.jpg";
 let photo2 = "https://i.postimg.cc/66df04mb/pi-karze-2016-6.jpg";
 
-function changeImage(obj) {
-    obj.src = (obj.src == photo1) ? photo2 : photo1;
-}
+buttonElement.addEventListener("click", () => {
+    imageElement.src = imageElement.src == photo1 ? photo2 : photo1;
+})
