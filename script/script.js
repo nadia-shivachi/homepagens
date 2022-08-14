850e2f3aab13d7445a112c3cac34ed83afbd18e3
Nadia Shivachi <nadia.shivachi@gmail.com>
2022-08-14 16:43:00 +0200

    JS code changed to arrow functions

--- script/script.js
+++ script/script.js
@@ -1,11 +1,18 @@
-console.log("Hej! strona o mnie. Zapraszam do zapoznania się :)")
+{
+  const welcome = () => {
+    console.log("Hej! strona o mnie. Zapraszam do zapoznania się :)");
+  };
+  welcome();
 
-let imageElement = document.querySelector(".js-image");
-let buttonElement = document.querySelector(".js-button");
+  const photoChange = () => {
+    const buttonElement = document.querySelector(".js-button");
+    const imageElement = document.querySelector(".js-image");
+    const photo1 = "https://i.postimg.cc/JnCDkf3B/IMG-20160831-173342.jpg";
+    const photo2 = "https://i.postimg.cc/66df04mb/pi-karze-2016-6.jpg";
 
-let photo1 = "https://i.postimg.cc/JnCDkf3B/IMG-20160831-173342.jpg";
-let photo2 = "https://i.postimg.cc/66df04mb/pi-karze-2016-6.jpg";
-
-buttonElement.addEventListener("click", () => {
-    imageElement.src = imageElement.src == photo1 ? photo2 : photo1;
-})
+    buttonElement.addEventListener("click", () => {
+      imageElement.src = imageElement.src == photo1 ? photo2 : photo1;
+    });
+  };
+  photoChange();
+}
